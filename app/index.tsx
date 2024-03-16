868fd184d5d5fb802d5615ac98b298ec5c0ab180
emreunlu <unlcemre0@gmail.com>
2024-03-16 03:12:55 +0300

    expo router, tailwindcss init

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-indigo-100">
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
